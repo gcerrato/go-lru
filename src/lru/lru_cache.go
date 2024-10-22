d10bbca374eb079a8c5e1927336b3babb30150d0
Gabriel Cerrato <gmcerrato1@gmail.com>
2024-10-22 16:44:37 +0200

    simplify goroutines and use locks instead of channels

--- src/lru/lru_cache.go
+++ src/lru/lru_cache.go
@@ -3,18 +3,13 @@ package lru
 import (
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 )
 
 type LRUCacheConfig struct {
 	ItemLimit int64
 	TTL       int64
-}
-
-type message[T any] struct {
-	value  T
-	key    string
-	action string
 }
 
 type LRUCacher[T any] interface {
@@ -28,6 +23,15 @@ type StorageItem[T any] struct {
 	DeleteAt time.Time
 }
 
+type SafeMap[T any] struct {
+	SafeMap map[string]*StorageItem[T]
+	mu      sync.RWMutex
+}
+
+func NewSafeMap[T any]() *SafeMap[T] {
+	return &SafeMap[T]{SafeMap: make(map[string]*StorageItem[T])}
+}
+
 func (item *StorageItem[T]) bumpDeleteAt(ms int64) *StorageItem[T] {
 	item.DeleteAt = time.Now().Add(time.Duration(ms) * time.Millisecond)
 	return item
@@ -35,14 +39,13 @@ func (item *StorageItem[T]) bumpDeleteAt(ms int64) *StorageItem[T] {
 
 type InMemoryLRUCache[T any] struct {
 	Config  LRUCacheConfig
-	Storage map[string]*StorageItem[T]
-
-	// channel operations
-	messageChannel chan message[T]
+	Storage *SafeMap[T]
 }
 
-func (cache InMemoryLRUCache[T]) Has(key string) bool {
-	storageItem, exists := cache.Storage[key]
+func (cache *InMemoryLRUCache[T]) Has(key string) bool {
+	cache.Storage.mu.Lock()
+	defer cache.Storage.mu.Unlock()
+	storageItem, exists := cache.Storage.SafeMap[key]
 	if !exists {
 		return false
 	}
@@ -50,8 +53,10 @@ func (cache InMemoryLRUCache[T]) Has(key string) bool {
 	return exists
 }
 
-func (cache InMemoryLRUCache[T]) Get(key string) (T, error) {
-	storageItem, exists := cache.Storage[key]
+func (cache *InMemoryLRUCache[T]) Get(key string) (T, error) {
+	cache.Storage.mu.Lock()
+	defer cache.Storage.mu.Unlock()
+	storageItem, exists := cache.Storage.SafeMap[key]
 	var zero T
 	if !exists {
 		return zero, errors.New("key not found on LRU cache")
@@ -60,71 +65,50 @@ func (cache InMemoryLRUCache[T]) Get(key string) (T, error) {
 	return storageItem.Value, nil
 }
 
-func (cache InMemoryLRUCache[T]) Set(key string, value T) T {
-	cache.messageChannel <- message[T]{key: key, value: value, action: "add"}
-	<-cache.messageChannel
+func (cache *InMemoryLRUCache[T]) Set(key string, value T) T {
+	cache.Storage.mu.Lock()
+	defer cache.Storage.mu.Unlock()
+	if int64(len(cache.Storage.SafeMap)) >= cache.Config.ItemLimit {
+		cache.removeOldestKey()
+	}
+
+	cache.Storage.SafeMap[key] = &StorageItem[T]{Value: value, DeleteAt: time.Now().Add(time.Duration(cache.Config.TTL) * time.Millisecond)}
 	return value
 }
 
-func (cache InMemoryLRUCache[T]) sweepKeys() {
+func (cache *InMemoryLRUCache[T]) sweepKeys() {
 	now := time.Now()
-	for key, value := range cache.Storage {
+	for key, value := range cache.Storage.SafeMap {
 		diff := now.Sub(value.DeleteAt).Milliseconds()
 		if diff >= 0 {
 			fmt.Printf("deleted key automatically %s with diff %d \n", key, diff)
-			delete(cache.Storage, key)
+			delete(cache.Storage.SafeMap, key)
 		}
 	}
 }
 
-func (cache InMemoryLRUCache[T]) removeOldestKey() {
+func (cache *InMemoryLRUCache[T]) removeOldestKey() {
 	var oldestKey string
 	var oldestTime time.Time
 
-	// Iterate over all keys to find the oldest one
-	for key, value := range cache.Storage {
-		// If this is the first key or an older key, update the oldest key tracker
+	for key, value := range cache.Storage.SafeMap {
 		if oldestKey == "" || value.DeleteAt.Before(oldestTime) {
 			oldestKey = key
 			oldestTime = value.DeleteAt
 		}
 	}
 
-	// Remove the oldest key if it exists and is due for deletion
 	if oldestKey != "" {
 		fmt.Printf("deleted oldest key %s \n", oldestKey)
-		delete(cache.Storage, oldestKey)
+		delete(cache.Storage.SafeMap, oldestKey)
 	}
 }
 
 func (cache *InMemoryLRUCache[T]) startMessageListener(interval time.Duration) {
-	cache.messageChannel = make(chan message[T])
-	go func() {
-		for {
-			time.Sleep(interval)
-			cache.messageChannel <- message[T]{action: "sweep"}
-		}
-	}()
-	go func() {
-		for msg := range cache.messageChannel {
-			if msg.action == "add" {
-				cache.setFromChannel(msg.key, msg.value)
-				cache.messageChannel <- message[T]{action: "done"}
-			} else if msg.action == "sweep" {
-				cache.sweepKeys()
-			}
-		}
-	}()
-}
-
-func (cache InMemoryLRUCache[T]) setFromChannel(key string, value T) T {
-
-	if int64(len(cache.Storage)) >= cache.Config.ItemLimit {
-		cache.removeOldestKey()
+	for {
+		time.Sleep(interval)
+		cache.sweepKeys()
 	}
-
-	cache.Storage[key] = &StorageItem[T]{Value: value, DeleteAt: time.Now().Add(time.Duration(cache.Config.TTL) * time.Millisecond)}
-	return value
 }
 
 type LRUCacheProvider[T any] interface {
@@ -136,7 +120,8 @@ type LRUCacheProvider[T any] interface {
 type InMemoryLRUCacheProvider[T any] struct{}
 
 func (cacheProvider InMemoryLRUCacheProvider[T]) NewLRUCache(config LRUCacheConfig) LRUCacher[T] {
-	cache := InMemoryLRUCache[T]{Config: config, Storage: make(map[string]*StorageItem[T])}
-	cache.startMessageListener(50 * time.Millisecond)
-	return cache
+	safeMap := NewSafeMap[T]()
+	cache := InMemoryLRUCache[T]{Config: config, Storage: safeMap}
+	go cache.startMessageListener(50 * time.Millisecond)
+	return &cache
 }
